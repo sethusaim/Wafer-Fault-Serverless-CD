9f054a977cb820ee8807691ce1a0e222c1143b79
sethusaim <sethusaim@gmail.com>
2022-08-19 11:38:07 +0530

    minor fix

--- lambda_functions/clustering/clustering_function.tf
+++ lambda_functions/clustering/clustering_function.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "test_lambda" {
   function_name = var.clustering_function_name
   role          = aws_iam_role.lambda_iam.arn
   package_type  = var.clustering_package_type
-  image_uri     = data.local_file.test_lambda_uri_file.content
+  image_uri     = data.local_file.clustering_lambda_uri_file.content
 }
 
 data "local_file" "clustering_lambda_uri_file" {
--- lambda_functions/data_transform_pred/data_transform_pred_function.tf
+++ lambda_functions/data_transform_pred/data_transform_pred_function.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "test_lambda" {
   function_name = var.data_transform_pred_function_name
   role          = aws_iam_role.lambda_iam.arn
   package_type  = var.data_transform_pred_package_type
-  image_uri     = data.local_file.test_lambda_uri_file.content
+  image_uri     = data.local_file.data_transform_lambda_uri_file
 }
 
 data "local_file" "data_transform_lambda_uri_file" {
