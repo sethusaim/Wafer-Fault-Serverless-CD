85d64c28fef26c6b9aaf3026007d1fbc1b913f42
sethusaim <sethusaim@gmail.com>
2022-08-19 11:55:32 +0530

    added lambda function modules to module.tf

--- module.tf
+++ module.tf
@@ -9,3 +9,47 @@ terraform {
 module "clustering_lambda_function" {
   source = "./lambda_functions/clustering"
 }
+
+module "data_transform_pred_function" {
+  source = "./lambda_functions/data_transform_pred"
+}
+
+module "data_transform_train_function" {
+  souce = "./lambda_functions/data_transform_train"
+}
+
+module "db_operation_pred_function" {
+  source = "./lambda_functions/db_operation_pred"
+}
+
+module "db_operation_train_function" {
+  source = "./lambda_functions/db_operation_train"
+}
+
+module "load_prod_model_function" {
+  source = "./lambda_functions/load_prod_model"
+}
+
+module "model_prediction_function" {
+  source = "./lambda_functions/model_prediction"
+}
+
+module "model_training_function" {
+  source = "./lambda_functions/model_training"
+}
+
+module "pred_data_validation_function" {
+  source = "./lambda_functions/pred_data_validation"
+}
+
+module "train_data_validation_function" {
+  source = "./lambda_functions/train_data_validation"
+}
+
+module "preprocessing_pred_function" {
+  source = "./lambda_functions/preprocessing_pred"
+}
+
+module "preprocessing_train_function" {
+  source = "./lambda_functions/preprocessing_train"
+}
