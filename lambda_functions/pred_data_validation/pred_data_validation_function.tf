f4d8bcfbebda5d8bff764111875fc60cd454750e
sethusaim <sethusaim@gmail.com>
2022-08-20 18:09:58 +0530

    made new changes to pred data validation function

--- lambda_functions/pred_data_validation/pred_data_validation_function.tf
+++ lambda_functions/pred_data_validation/pred_data_validation_function.tf
@@ -2,9 +2,9 @@ resource "aws_lambda_function" "pred_data_validation_lambda" {
   function_name = var.pred_data_validation_function_name
   role          = aws_iam_role.lambda_iam.arn
   package_type  = var.pred_data_validation_package_type
-  image_uri     = data.local_file.pred_data_validation_lambda_uri_file.content
+  image_uri     = data.local_file.pred_data_validation_lambda_uri_file
 }
 
 data "local_file" "pred_data_validation_lambda_uri_file" {
-  filename = "./lambda_function_uri/pred_data_validation_uri.txt"
+  filename = "./lambda_function_manifests/wafer_pred_data_validation_uri.txt"
 }
