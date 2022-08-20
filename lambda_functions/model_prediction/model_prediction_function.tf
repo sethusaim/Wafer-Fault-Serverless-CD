4c05024a5d2d0da9abd3eaa7f43277dabc7bec3b
sethusaim <sethusaim@gmail.com>
2022-08-20 18:07:07 +0530

    made new changes to model prediction lambda function

--- lambda_functions/model_prediction/model_prediction_function.tf
+++ lambda_functions/model_prediction/model_prediction_function.tf
@@ -2,9 +2,9 @@ resource "aws_lambda_function" "model_prediction_lambda" {
   function_name = var.model_prediction_function_name
   role          = aws_iam_role.lambda_iam.arn
   package_type  = var.model_prediction_package_type
-  image_uri     = data.local_file.model_prediction_lambda_uri_file.content
+  image_uri     = data.local_file.model_prediction_lambda_uri_file
 }
 
 data "local_file" "model_prediction_lambda_uri_file" {
-  filename = "./lambda_function_uri/model_prediction_uri.txt"
+  filename = "./lambda_function_manifests/wafer_model_prediction_uri.txt"
 }
--- lambda_functions/model_prediction/variables.tf
+++ lambda_functions/model_prediction/variables.tf
@@ -8,7 +8,12 @@ variable "model_prediction_package_type" {
   type    = string
 }
 
-variable "lambda_iam_role_name" {
+variable "model_prediction_lambda_iam_role_name" {
   default = "lambda_iam_role"
   type    = string
+}
+
+variable "model_prediction_lambda_iam_role_arn" {
+  default = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  type = string
 }
