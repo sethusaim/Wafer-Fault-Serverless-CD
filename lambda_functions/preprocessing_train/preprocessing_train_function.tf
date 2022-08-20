ba1d38602885545970fea31a96b24e348320da18
sethusaim <sethusaim@gmail.com>
2022-08-20 18:12:07 +0530

    made new changes to preprocessing train function

--- lambda_functions/preprocessing_train/preprocessing_train_function.tf
+++ lambda_functions/preprocessing_train/preprocessing_train_function.tf
@@ -2,9 +2,9 @@ resource "aws_lambda_function" "preprocessing_train_lambda" {
   function_name = var.preprocessing_train_function_name
   role          = aws_iam_role.lambda_iam.arn
   package_type  = var.preprocessing_train_package_type
-  image_uri     = data.local_file.preprocessing_train_lambda_uri_file.content
+  image_uri     = data.local_file.preprocessing_train_lambda_uri_file
 }
 
 data "local_file" "preprocessing_train_lambda_uri_file" {
-  filename = "./lambda_function_uri/preprocessing_train_uri.txt"
+  filename = "./lambda_function_manifests/wafer_preprocessing_train_uri.txt"
 }
--- lambda_functions/preprocessing_train/variables.tf
+++ lambda_functions/preprocessing_train/variables.tf
@@ -8,7 +8,12 @@ variable "preprocessing_train_package_type" {
   type    = string
 }
 
-variable "lambda_iam_role_name" {
+variable "preprocessing_train_lambda_iam_role_name" {
   default = "lambda_iam_role"
   type    = string
+}
+
+variable "preprocessing_train_lambda_iam_role_arn" {
+  default = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  type = string
 }
