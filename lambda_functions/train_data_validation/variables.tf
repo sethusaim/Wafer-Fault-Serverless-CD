ac97c3b2a92ea54f2e2d21c81667056606d05f11
sethusaim <sethusaim@gmail.com>
2022-08-20 18:13:40 +0530

    made new changes to train data validation function

--- lambda_functions/train_data_validation/variables.tf
+++ lambda_functions/train_data_validation/variables.tf
@@ -8,7 +8,12 @@ variable "train_data_validation_package_type" {
   type    = string
 }
 
-variable "lambda_iam_role_name" {
+variable "train_data_validation_lambda_iam_role_name" {
   default = "lambda_iam_role"
   type    = string
+}
+
+variable "train_data_validation_lambda_iam_role_arn" {
+  default = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  type = string
 }
