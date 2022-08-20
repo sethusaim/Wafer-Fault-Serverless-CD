7e02c585f1fca7d498f25fe7a68e0eceecbafcc5
sethusaim <sethusaim@gmail.com>
2022-08-20 18:08:10 +0530

    made new changes to model training lambda function

--- lambda_functions/model_training/model_training_function.tf
+++ lambda_functions/model_training/model_training_function.tf
@@ -2,9 +2,9 @@ resource "aws_lambda_function" "model_training_lambda" {
   function_name = var.model_training_function_name
   role          = aws_iam_role.lambda_iam.arn
   package_type  = var.model_training_package_type
-  image_uri     = data.local_file.model_training_lambda_uri_file.content
+  image_uri     = data.local_file.model_training_lambda_uri_file
 }
 
 data "local_file" "model_training_lambda_uri_file" {
-  filename = "./lambda_function_uri/model_training_uri.txt"
+  filename = "./lambda_function_manifests/wafer_model_training_uri.txt"
 }
--- lambda_functions/model_training/variables.tf
+++ lambda_functions/model_training/variables.tf
@@ -8,7 +8,12 @@ variable "model_training_package_type" {
   type    = string
 }
 
-variable "lambda_iam_role_name" {
+variable "model_training_lambda_iam_role_name" {
   default = "lambda_iam_role"
   type    = string
+}
+
+variable "model_training_lambda_iam_role_arn" {
+  default = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  type = string
 }
