95360fce3c9bc7099a235af7515c77af8d16a40e
sethusaim <sethusaim@gmail.com>
2022-08-20 18:03:13 +0530

    made new changes to db operation pred lambda function

--- lambda_functions/db_operation_pred/db_operation_pred_function.tf
+++ lambda_functions/db_operation_pred/db_operation_pred_function.tf
@@ -2,9 +2,9 @@ resource "aws_lambda_function" "db_operation_pred_lambda" {
   function_name = var.db_operation_pred_function_name
   role          = aws_iam_role.lambda_iam.arn
   package_type  = var.db_operation_pred_package_type
-  image_uri     = data.local_file.db_operation_pred_lambda_uri_file.content
+  image_uri     = data.local_file.db_operation_pred_lambda_uri_file
 }
 
 data "local_file" "db_operation_pred_lambda_uri_file" {
-  filename = "./lambda_function_uri/db_operation_pred_uri.txt"
+  filename = "./lambda_function_manifests/wafer_db_operation_pred_uri.txt"
 }
